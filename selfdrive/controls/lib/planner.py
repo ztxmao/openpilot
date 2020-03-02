8bd0118ec5c347d3623f761994901b596e37afa8
Harald Schafer <harald.the.engineer@gmail.com>
2020-03-02 00:05:59 -0800

    importante

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -136,8 +136,8 @@ class Planner():
 
 
       desired_speed = speed + np.clip(distance/3.0, -speed*.1 - .5, speed*.1 + .5)
-      speed_change = desired_speed - self.v_model
-      desired_accel = np.clip((speed_change*FPS/2.0 + accel)/2.0, -ACCEL_LIMIT, ACCEL_LIMIT)
+      speed_change = desired_speed - v_ego
+      desired_accel = np.clip((speed_change/2.0 + accel)/2.0, -ACCEL_LIMIT, ACCEL_LIMIT)
 
       self.a_model = desired_accel
       self.v_model += self.a_model/FPS
